b53e62a1807d4e59ca0c509acf2bea1f37cd2e41
sahil.kharb <sahil.kharb@gojek.com>
2022-08-25 21:23:43 +0530

    calculates tax till 5 crore including surcharge and health cess

--- app/helpers/income_tax_calculator.rb
+++ app/helpers/income_tax_calculator.rb
@@ -1,22 +1,30 @@
 class IncomeTaxCalculator
+  NO_INCOME_TAX = 0.0
   MAX_NON_TAXABLE_INCOME_AMOUNT = 250000
   INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000 = 0.05
   INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000 = 0.2
   INCOME_TAX_RATE_FOR_INCOME_ABOVE_1000000 = 0.30
   SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_50L_AND_1C = 0.10
-  SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_1C_AND_2C = 0.20
+  SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_1C_AND_2C = 0.15
+  SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_2C_AND_5C = 0.25
   MAX_AMOUNT_IN_TAX_SLAB_1 = 500000
   MAX_AMOUNT_IN_TAX_SLAB_2 = 1000000
   MAX_AMOUNT_WITHOUT_SURCHARGE_TAX_1 = 5000000
   MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_1 = 10000000
+  MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_2 = 20000000
+  HEALTH_CESS_RATE = 0.04
 
   def initialize(total_income)
     @total_income = total_income
   end
 
   def get_tax
+    (calculate_income_tax_only + calculate_surcharge_tax_only) * (1 + HEALTH_CESS_RATE)
+  end
+
+  def calculate_income_tax_only
     if @total_income <= MAX_NON_TAXABLE_INCOME_AMOUNT
-      0.0
+      NO_INCOME_TAX
     elsif @total_income <= MAX_AMOUNT_IN_TAX_SLAB_1
       (@total_income - MAX_NON_TAXABLE_INCOME_AMOUNT) * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
     elsif @total_income <= MAX_AMOUNT_IN_TAX_SLAB_2
@@ -24,21 +32,33 @@ class IncomeTaxCalculator
     elsif @total_income <= MAX_AMOUNT_WITHOUT_SURCHARGE_TAX_1
       max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3
     elsif @total_income <= MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_1
-      max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3 +
-        surcharge_tax_on_income_tax_for_range_50_and_1C
+      income_tax = max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3
+      income_tax
+    elsif @total_income <= MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_2
+      income_tax = max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3
+      income_tax
     else
-      max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3 +
-        max_surcharge_tax_on_surcharge_tax_slab_1 + surcharge_tax_on_income_tax_amount_range_1C_and_2C
+      income_tax = max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3
+      income_tax
+    end
+  end
+
+  def calculate_surcharge_tax_only
+    if @total_income <= MAX_AMOUNT_WITHOUT_SURCHARGE_TAX_1
+      0
+    elsif @total_income <= MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_1
+      income_tax = max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3
+      income_tax * SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_50L_AND_1C
+    elsif @total_income <= MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_2
+      income_tax = max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3
+      income_tax * SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_1C_AND_2C
+    else
+      income_tax = max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_in_tax_slab_3
+      income_tax * SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_2C_AND_5C
     end
   end
 
   private
-
-  def surcharge_tax_on_income_tax_amount_range_1C_and_2C
-    income_amount_eligible_for_surcharge_tax_2 = @total_income - MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_1
-    income_tax_amount_eligible_for_surcharge_tax_2 = income_amount_eligible_for_surcharge_tax_2 * INCOME_TAX_RATE_FOR_INCOME_ABOVE_1000000
-    income_tax_amount_eligible_for_surcharge_tax_2 * SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_1C_AND_2C
-  end
 
   def income_tax_in_tax_slab_2
     (@total_income - MAX_AMOUNT_IN_TAX_SLAB_1) * INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000
@@ -54,17 +74,5 @@ class IncomeTaxCalculator
 
   def max_income_tax_in_tax_slab_1
     (MAX_AMOUNT_IN_TAX_SLAB_1 - MAX_NON_TAXABLE_INCOME_AMOUNT) * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
-  end
-
-  def max_surcharge_tax_on_surcharge_tax_slab_1
-    income_amount_eligible_for_surcharge_tax_1 = MAX_AMOUNT_IN_SURCHARGE_TAX_SLAB_1 - MAX_AMOUNT_WITHOUT_SURCHARGE_TAX_1
-    income_tax_amount_eligible_for_surcharge_tax_1 = income_amount_eligible_for_surcharge_tax_1 * INCOME_TAX_RATE_FOR_INCOME_ABOVE_1000000
-    income_tax_amount_eligible_for_surcharge_tax_1 * SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_50L_AND_1C
-  end
-
-  def surcharge_tax_on_income_tax_for_range_50_and_1C
-    income_amount_eligible_for_surcharge_tax_1 = @total_income - MAX_AMOUNT_WITHOUT_SURCHARGE_TAX_1
-    income_tax_amount_eligible_for_surcharge_tax_1 = income_amount_eligible_for_surcharge_tax_1 * INCOME_TAX_RATE_FOR_INCOME_ABOVE_1000000
-    income_tax_amount_eligible_for_surcharge_tax_1 * SURCHARGE_TAX_RATE_FOR_INCOME_RANGE_50L_AND_1C
   end
 end
--- spec/helpers/income_tax_calculator_spec.rb
+++ spec/helpers/income_tax_calculator_spec.rb
@@ -12,76 +12,96 @@ RSpec.describe IncomeTaxCalculator, type: :helper do
   end
 
   context 'when total income is more than minimum taxable income but less than 50 lacs' do
-    it 'returns 5% of income for the range between 2.5 lac to 5 lacs' do
-      income_tax = IncomeTaxCalculator.new(450000)
+    it 'returns 5% of income for the range between 2.5 lac to 5 lacs + health cess' do
+      income_tax_calculator = IncomeTaxCalculator.new(450000)
 
-      minimum_taxable_income = income_tax.get_tax
+      actual_income_tax = income_tax_calculator.get_tax
 
-      expect(minimum_taxable_income).to be_eql(10000.0)
+      expect(actual_income_tax).to eq(10400.0)
     end
 
-    it 'returns additional 20% of income for range between 5 lacs to 10 lacs' do
+    it 'returns additional 20% of income for range between 5 lacs to 10 lacs + health cess' do
       taxable_amount = 950000
       extra_amount_in_this_income_range = taxable_amount - 500000
       another_income_tax_calculator = IncomeTaxCalculator.new(500000)
       income_tax_on_500000 = another_income_tax_calculator.get_tax
-      total_expected_income_tax = income_tax_on_500000 + extra_amount_in_this_income_range * 0.20
+      expected_total_tax = income_tax_on_500000 + (extra_amount_in_this_income_range * 0.20 * 1.04)
 
       income_tax_calculator = IncomeTaxCalculator.new(taxable_amount)
-      minimum_taxable_income = income_tax_calculator.get_tax
+      actual_tax = income_tax_calculator.get_tax
 
-      expect(minimum_taxable_income).to be_eql(total_expected_income_tax)
+      expect(actual_tax).to eq(expected_total_tax)
     end
 
-    it 'returns additional 30% of income for all income above 10 lacs' do
+    it 'returns additional 30% of income for all income above 10 lacs + health cess' do
       total_income_amount = 1500000
       TEN_LACS = 1000000
       extra_income_in_this_income_range = total_income_amount - TEN_LACS
       income_tax_calculator_till_10_lac = IncomeTaxCalculator.new(TEN_LACS)
       income_tax_on_10_lac = income_tax_calculator_till_10_lac.get_tax
-      expected_taxable_income = income_tax_on_10_lac + extra_income_in_this_income_range * 0.30
+      expected_tax = income_tax_on_10_lac + extra_income_in_this_income_range * 0.30 * 1.04
 
       income_tax_calculator = IncomeTaxCalculator.new(total_income_amount)
-      taxable_income = income_tax_calculator.get_tax
+      actual_tax = income_tax_calculator.get_tax
 
-      expect(taxable_income).to be_eql(expected_taxable_income)
+      expect(actual_tax).to be_eql(expected_tax)
     end
   end
 
   context 'when total income is more than 50 lacs' do
-    it 'returns additional 10% surcharge on income tax on amount between 50 lacs and 1 crore' do
+    it 'returns additional 10% surcharge_tax on income tax on amount between 50 lacs and 1 crore + health cess' do
       total_income_amount = 7500000
       FIFTY_LACS = 5000000
       extra_income_in_this_income_range = total_income_amount - FIFTY_LACS
       income_tax_calculator_till_50_lacs = IncomeTaxCalculator.new(FIFTY_LACS)
-      income_tax_on_50_lac = income_tax_calculator_till_50_lacs.get_tax
+      income_tax_on_50_lac = income_tax_calculator_till_50_lacs.calculate_income_tax_only
 
       income_tax_on_extra_income_in_this_range = extra_income_in_this_income_range * 0.30
-      surcharge_tax_on_extra_income_in_this_range = income_tax_on_extra_income_in_this_range * 0.10
-
-      expected_income_tax = income_tax_on_50_lac + income_tax_on_extra_income_in_this_range + surcharge_tax_on_extra_income_in_this_range
+      total_income_tax = income_tax_on_50_lac + income_tax_on_extra_income_in_this_range
+      surcharge_tax = total_income_tax * 0.10
+      expected_income_tax = (total_income_tax + surcharge_tax) * 1.04
 
       income_tax_calculator = IncomeTaxCalculator.new(total_income_amount)
       actual_income_tax = income_tax_calculator.get_tax
 
-      expect(actual_income_tax).to equal(expected_income_tax)
+      expect(actual_income_tax).to eq(expected_income_tax)
     end
-    it 'returns additional 20% surcharge on income tax on amount between 1 crore and 2 crore' do
+    it 'returns additional 15% surcharge_tax on income tax on amount between 1 crore and 2 crore + health cess' do
       total_income_amount = 17500000
       ONE_CRORE = 10000000
       extra_income_in_this_income_range = total_income_amount - ONE_CRORE
       income_tax_calculator_till_one_crore = IncomeTaxCalculator.new(ONE_CRORE)
-      income_tax_on_one_crore = income_tax_calculator_till_one_crore.get_tax
+      income_tax_on_one_crore = income_tax_calculator_till_one_crore.calculate_income_tax_only
 
       income_tax_on_extra_income_in_this_range = extra_income_in_this_income_range * 0.30
-      surcharge_tax_on_extra_income_in_this_range = income_tax_on_extra_income_in_this_range * 0.20
-
-      expected_income_tax = income_tax_on_one_crore + income_tax_on_extra_income_in_this_range + surcharge_tax_on_extra_income_in_this_range
+      total_income_tax = income_tax_on_one_crore + income_tax_on_extra_income_in_this_range
+      surcharge_tax = (total_income_tax) * 0.15
+      income_tax_and_surcharge = total_income_tax + surcharge_tax
+      expected_income_tax = (income_tax_and_surcharge) * 1.04
 
       income_tax_calculator = IncomeTaxCalculator.new(total_income_amount)
       actual_income_tax = income_tax_calculator.get_tax
 
-      expect(actual_income_tax).to equal(expected_income_tax)
+      expect(actual_income_tax).to eq(expected_income_tax)
+    end
+
+    it 'returns additional 25% surcharge_tax on income tax on amount between 2 crore and 5 crore' do
+      total_income_amount = 47500000
+      TWO_CRORE = 20000000
+      extra_income_in_this_income_range = total_income_amount - TWO_CRORE
+      income_tax_calculator_till_two_crore = IncomeTaxCalculator.new(TWO_CRORE)
+      income_tax_on_two_crore = income_tax_calculator_till_two_crore.calculate_income_tax_only
+
+      income_tax_on_extra_income_in_this_range = extra_income_in_this_income_range * 0.30
+      total_income_tax = income_tax_on_two_crore + income_tax_on_extra_income_in_this_range
+      surcharge_amount = total_income_tax * 0.25
+
+      expected_income_tax = (income_tax_on_two_crore + income_tax_on_extra_income_in_this_range + surcharge_amount) * 1.04
+
+      income_tax_calculator = IncomeTaxCalculator.new(total_income_amount)
+      actual_income_tax = income_tax_calculator.get_tax
+
+      expect(actual_income_tax).to eq(expected_income_tax)
     end
   end
 end
