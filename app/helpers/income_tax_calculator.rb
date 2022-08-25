ea157866ec26347c16a9211122dc336263194f7d
sahil.kharb <sahil.kharb@gojek.com>
2022-08-25 18:21:08 +0530

    REFACTOR: extracts max tax calculation for slab 1 and 2

--- app/helpers/income_tax_calculator.rb
+++ app/helpers/income_tax_calculator.rb
@@ -16,19 +16,23 @@ class IncomeTaxCalculator
     elsif @total_income <= MAX_AMOUNT_IN_TAX_SLAB_1
       (@total_income - MAX_NON_TAXABLE_INCOME_AMOUNT) * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
     elsif @total_income <= MAX_AMOUNT_IN_TAX_SLAB_2
-      max_income_in_tax_slab_1 = MAX_AMOUNT_IN_TAX_SLAB_1 - MAX_NON_TAXABLE_INCOME_AMOUNT
-      max_income_tax_in_tax_slab_1 = max_income_in_tax_slab_1 * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
       income_in_tax_slab_2 = @total_income - MAX_AMOUNT_IN_TAX_SLAB_1
       income_tax_for_tax_slab_2 = income_in_tax_slab_2 * INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000
       max_income_tax_in_tax_slab_1 + income_tax_for_tax_slab_2
     else
-      max_income_in_tax_slab_1 = MAX_AMOUNT_IN_TAX_SLAB_1 - MAX_NON_TAXABLE_INCOME_AMOUNT
-      max_income_tax_in_tax_slab_1 = max_income_in_tax_slab_1 * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
-      max_income_in_tax_slab_2 = MAX_AMOUNT_IN_TAX_SLAB_2 - MAX_AMOUNT_IN_TAX_SLAB_1
-      max_income_tax_in_tax_slab_2 = max_income_in_tax_slab_2 * INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000
       income_in_tax_slab_3 = @total_income - MAX_AMOUNT_IN_TAX_SLAB_2
       income_tax_for_tax_slab_3 = income_in_tax_slab_3 * INCOME_TAX_RATE_FOR_INCOME_ABOVE_1000000
       max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_for_tax_slab_3
     end
+  end
+
+  private
+
+  def max_income_tax_in_tax_slab_2
+    (MAX_AMOUNT_IN_TAX_SLAB_2 - MAX_AMOUNT_IN_TAX_SLAB_1) * INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000
+  end
+
+  def max_income_tax_in_tax_slab_1
+    (MAX_AMOUNT_IN_TAX_SLAB_1 - MAX_NON_TAXABLE_INCOME_AMOUNT) * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
   end
 end
