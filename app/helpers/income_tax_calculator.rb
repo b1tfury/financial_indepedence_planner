8226ebad31c4a85b84a05889927dcb63bdf0284d
sahil.kharb <sahil.kharb@gojek.com>
2022-08-25 17:35:30 +0530

    income tax calculator can calculate income tax for income range upto 5lacs

--- app/helpers/income_tax_calculator.rb
+++ app/helpers/income_tax_calculator.rb
@@ -1,8 +1,16 @@
 class IncomeTaxCalculator
+  MAX_NON_TAXABLE_INCOME_AMOUNT = 250000
+  INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000 = 0.05
 
-  def initialize(total_income) end
+  def initialize(total_income)
+    @total_income = total_income
+  end
 
   def calculate_minimum_taxable_income
-    0
+    if @total_income <= MAX_NON_TAXABLE_INCOME_AMOUNT
+      0
+    else
+      (@total_income - MAX_NON_TAXABLE_INCOME_AMOUNT) * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
+    end
   end
 end
--- spec/helpers/income_tax_calculator_spec.rb
+++ spec/helpers/income_tax_calculator_spec.rb
@@ -3,11 +3,22 @@ require 'rails_helper'
 RSpec.describe IncomeTaxCalculator, type: :helper do
   context 'when total income is less than minimum taxable income' do
     it 'total tax on total income should be 0' do
-      income_tax = IncomeTaxCalculator.new('total_income' => 50000)
+      income_tax = IncomeTaxCalculator.new(50000)
 
       minimum_taxable_income = income_tax.calculate_minimum_taxable_income
 
-      expect(minimum_taxable_income).to be(0)
+      expect(minimum_taxable_income).to be_eql(0.0)
     end
+  end
+
+  context 'when total income is more than minimum taxable income' do
+    it 'returns 5% of income for the range between 2.5 lac to 5 lacs' do
+      income_tax = IncomeTaxCalculator.new(450000)
+
+      minimum_taxable_income = income_tax.calculate_minimum_taxable_income
+
+      expect(minimum_taxable_income).to be_eql(10000.0)
+    end
+
   end
 end
