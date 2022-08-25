68e602e26a67b4dc2b57b0213f3bc8c43d0baae7
sahil.kharb <sahil.kharb@gojek.com>
2022-08-25 18:17:40 +0530

    income tax calculator now calculate tax when income is more than 10 lacs

--- app/helpers/income_tax_calculator.rb
+++ app/helpers/income_tax_calculator.rb
@@ -2,7 +2,9 @@ class IncomeTaxCalculator
   MAX_NON_TAXABLE_INCOME_AMOUNT = 250000
   INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000 = 0.05
   INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000 = 0.2
+  INCOME_TAX_RATE_FOR_INCOME_ABOVE_1000000 = 0.30
   MAX_AMOUNT_IN_TAX_SLAB_1 = 500000
+  MAX_AMOUNT_IN_TAX_SLAB_2 = 1000000
 
   def initialize(total_income)
     @total_income = total_income
@@ -13,12 +15,20 @@ class IncomeTaxCalculator
       0.0
     elsif @total_income <= MAX_AMOUNT_IN_TAX_SLAB_1
       (@total_income - MAX_NON_TAXABLE_INCOME_AMOUNT) * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
-    else
+    elsif @total_income <= MAX_AMOUNT_IN_TAX_SLAB_2
       max_income_in_tax_slab_1 = MAX_AMOUNT_IN_TAX_SLAB_1 - MAX_NON_TAXABLE_INCOME_AMOUNT
       max_income_tax_in_tax_slab_1 = max_income_in_tax_slab_1 * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
       income_in_tax_slab_2 = @total_income - MAX_AMOUNT_IN_TAX_SLAB_1
       income_tax_for_tax_slab_2 = income_in_tax_slab_2 * INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000
       max_income_tax_in_tax_slab_1 + income_tax_for_tax_slab_2
+    else
+      max_income_in_tax_slab_1 = MAX_AMOUNT_IN_TAX_SLAB_1 - MAX_NON_TAXABLE_INCOME_AMOUNT
+      max_income_tax_in_tax_slab_1 = max_income_in_tax_slab_1 * INCOME_TAX_RATE_FOR_INCOME_RANGE_250000_AND_500000
+      max_income_in_tax_slab_2 = MAX_AMOUNT_IN_TAX_SLAB_2 - MAX_AMOUNT_IN_TAX_SLAB_1
+      max_income_tax_in_tax_slab_2 = max_income_in_tax_slab_2 * INCOME_TAX_RATE_FOR_INCOME_RANGE_500000_AND_1000000
+      income_in_tax_slab_3 = @total_income - MAX_AMOUNT_IN_TAX_SLAB_2
+      income_tax_for_tax_slab_3 = income_in_tax_slab_3 * INCOME_TAX_RATE_FOR_INCOME_ABOVE_1000000
+      max_income_tax_in_tax_slab_1 + max_income_tax_in_tax_slab_2 + income_tax_for_tax_slab_3
     end
   end
 end
--- spec/helpers/income_tax_calculator_spec.rb
+++ spec/helpers/income_tax_calculator_spec.rb
@@ -23,14 +23,29 @@ RSpec.describe IncomeTaxCalculator, type: :helper do
     it 'returns additional 20% of income for range between 5 lacs to 10 lacs' do
       taxable_amount = 950000
       extra_amount_in_this_income_range = taxable_amount - 500000
-      income_tax_calculator = IncomeTaxCalculator.new(taxable_amount)
       another_income_tax_calculator = IncomeTaxCalculator.new(500000)
       income_tax_on_500000 = another_income_tax_calculator.calculate_minimum_taxable_income
       total_expected_income_tax = income_tax_on_500000 + extra_amount_in_this_income_range * 0.20
 
+      income_tax_calculator = IncomeTaxCalculator.new(taxable_amount)
       minimum_taxable_income = income_tax_calculator.calculate_minimum_taxable_income
 
       expect(minimum_taxable_income).to be_eql(total_expected_income_tax)
     end
+
+    it 'returns additional 30% of income for all income above 10 lacs' do
+      total_income_amount = 1500000
+      TEN_LACS = 1000000
+      extra_income_in_this_income_range = total_income_amount - TEN_LACS
+      income_tax_calculator_till_10_lac = IncomeTaxCalculator.new(TEN_LACS)
+      income_tax_on_10_lac = income_tax_calculator_till_10_lac.calculate_minimum_taxable_income
+      expected_taxable_income = income_tax_on_10_lac + extra_income_in_this_income_range * 0.30
+
+      income_tax_calculator = IncomeTaxCalculator.new(total_income_amount)
+      taxable_income = income_tax_calculator.calculate_minimum_taxable_income
+
+      expect(taxable_income).to be_eql(expected_taxable_income)
+    end
+
   end
 end
